b28c3f457b8c2261ce4a4619822c80dbe3319792
congphuc <phuccog@gmail.com>
2020-09-16 22:17:28 +0700

    push

--- app/Controllers/Http/OfficalAPIController.js
+++ app/Controllers/Http/OfficalAPIController.js
@@ -18,30 +18,30 @@ class OfficalAPIController {
    */
   async webhook({ request, response }) {
     const data = request.all();
-    const user_id = data.sender.id;
-    console.log(data);
-    switch(data.event_name){
-      case "user_send_text":
-        switch(data.message.text){
-          case "#contributors":
-            OfiicalServices.contributors(user_id)
-            break;
-          case "#subjects":
-            OfiicalServices.subjects(user_id);
-            break;
-          case "#courses":
-            OfiicalServices.courses(user_id)
-            break;
-          default: 
-            // if("#courseBySubjects" == data.message.text.split("-")[0]){
-            //   OfiicalServices.coursesBySubject(user_id , +data.message.text.split("-")[1])
-            //   break;
-            // }
-            OfiicalServices.default(user_id)
-        }
-        break;
+    // const user_id = data.sender.id;
+    // console.log(data);
+    // switch(data.event_name){
+    //   case "user_send_text":
+    //     switch(data.message.text){
+    //       case "#contributors":
+    //         OfiicalServices.contributors(user_id)
+    //         break;
+    //       case "#subjects":
+    //         OfiicalServices.subjects(user_id);
+    //         break;
+    //       case "#courses":
+    //         OfiicalServices.courses(user_id)
+    //         break;
+    //       default: 
+    //         // if("#courseBySubjects" == data.message.text.split("-")[0]){
+    //         //   OfiicalServices.coursesBySubject(user_id , +data.message.text.split("-")[1])
+    //         //   break;
+    //         // }
+    //         OfiicalServices.default(user_id)
+    //     }
+    //     break;
 
-    }
+    // }
     return response.send({});
   }
 }
